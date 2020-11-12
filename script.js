622bc192f6a798f9e1df60e7643e5a83b7159b2b
Joe Maniaci <joe.maniaci1@gmail.com>
2020-11-12 17:53:09 -0500

    added the submit button click event and if/else that sets locally stored values

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 
+//global variables
 var currentTime = moment().format("HH");
 var currentDayParagraph = $("#currentDay");
 var textArea = $("textarea");
+var buttons = $("button");
+buttonId = "";
 
 
 //sets the current day and time 
 currentDayParagraph.text(moment().format("LLLL"));
 
-
-//changes text area color based on time of day
+//sets function for all textarea elements
 textArea.each(function () {
 
   var textAreaTime = $(this).attr("data-time");
   var formattedTime = moment(textAreaTime, "HH").format("HH");
 
+  //changes text area color based on time of day
   if (formattedTime < currentTime) {
     $(this).addClass("past");
   } else if (formattedTime > currentTime) {
@@ -22,5 +25,31 @@ textArea.each(function () {
     $(this).addClass("present");
 
   }
+
+  //Sets textarea to locally stored values
+  if (!localStorage.getItem(textAreaTime)) {
+    return;
+  } else {
+    $(this).val(localStorage.getItem(textAreaTime));
+  }
+
 })
+
+//submit button click event
+buttons.each(function (event) {
+
+  $(this).on("click", function () {
+
+    //sets the inputted text as a variable
+    buttonId = $(this).attr("id");
+    selectedTextArea = $("." + buttonId);
+    inputText = selectedTextArea.val();
+    //inputText = $(this).prev("textarea").val();
+
+    //locall stores the input text
+    localStorage.setItem(buttonId, inputText);
+
+  })
+})
+
 
